a8ab62fa2632a286980f723f4e0bcf7a87d10af1
Fisheep1207 <qiuyuyang@Fisheep.local>
2020-12-31 15:04:16 +0800

    add stall to PC.v

--- codes/PC.v
+++ codes/PC.v
@@ -3,6 +3,7 @@ module PC
     clk_i,
     rst_i,
     start_i,
+    stall_i,
     PCWrite_i,
     pc_i,
     pc_o
@@ -12,6 +13,7 @@ module PC
 input               clk_i;
 input               rst_i;
 input               start_i;
+input               stall_i;
 input               PCWrite_i;
 input   [31:0]      pc_i;
 output  [31:0]      pc_o;
@@ -25,11 +27,11 @@ always@(posedge clk_i or posedge rst_i) begin
         pc_o <= 32'b0;
     end  
     else begin
-        if (PCWrite_i) begin
+        if (~stall_i && PCWrite_i) begin
             if(start_i)
                 pc_o <= pc_i;
             else
-                pc_o <= pc_o;
+                pc_o <= 32'b0;
         end
     end
 end
